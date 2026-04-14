b28887c75e037b9e4322200754414747fa8f727a
MajidZanjani <majidmmz1972@gmail.com>
2026-04-14 20:53:46 +0400

    feat: task create functionality

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,12 +2,28 @@ const API_URL = "http://localhost:4000/api/tasks";
 
 export async function getTasks() {
   const response = await fetch(API_URL);
-
   if (!response.ok) {
     throw new Error(
       "Failed to fetch tasks from API. Please check your connection.",
     );
   }
-
   return response.json();
+}
+
+export async function createTask(taskData) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "content-Type": "application/json",
+    },
+    body: JSON.stringify(taskData),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to create task.");
+  }
+
+  return data;
 }
